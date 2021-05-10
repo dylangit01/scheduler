9736da8e7f15408941e8f611ed94c1406bb26d41
Dylan <dcren2010@gmail.com>
2021-05-10 13:38:07 -0400

    sbook run tests for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,15 @@ import React from "react";
 
 import "components/Button.scss";
 
-export default function Button(props) {
-   return <></>;
+export default function Button({ children, onClick, disabled }) {
+	let buttonClass = 'button';
+	if (children) {
+		buttonClass += ` button--${children === 'Cancel' ? 'danger' : children.toLowerCase()}`;
+	}
+
+	return (
+		<button disabled={disabled} onClick={onClick} className={buttonClass}>
+			{children}
+		</button>
+	);
 }
