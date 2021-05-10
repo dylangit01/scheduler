ac7dcbd5d3118d5fbc400a13f022e8eeca0010d9
Dylan <dcren2010@gmail.com>
2021-05-10 18:38:29 -0400

    adding state to the app

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import DayList from './DayList'
+import React, { useState } from 'react';
+import DayList from './DayList';
 
 import 'components/Application.scss';
 
@@ -22,18 +22,34 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [selectedDay, setSelectedDay] = useState('Monday');
 	return (
 		<main className='layout'>
 			<section className='sidebar'>
 				<img className='sidebar--centered' src='images/logo.png' alt='Interview Scheduler' />
 				<hr className='sidebar__separator sidebar--centered' />
 				<nav className='sidebar__menu'>
-          <DayList days={days} day={'Monday'} setDay={(name)=>{console.log(name)}} />
-        </nav>
+					{/* setDay fn has been passed down to DayList, and passed down to DayListItem again, because the trigger event is from DayListItem, the obtained day value will be retrieved from DayListItem setDay fn, and logged here.  */}
+					{/*
+          //////////////////////////////////////////////////////////////////////////////////
+          //  Using useState to update the setDay fn, since it accepts (day name) as argument,
+          //  it can be simple as: setDay={setSelectedDay}
+          //
+          //  setDay={(name) => {
+					//		setSelectedDay(name);
+					//	}}
+          ////////////////////////////////////////////////////
+          */}
+					<DayList
+						days={days}
+						day={selectedDay}
+						setDay={setSelectedDay}
+					/>
+				</nav>
 				<img className='sidebar__lhl sidebar--centered' src='images/lhl.png' alt='Lighthouse Labs' />
 			</section>
 			<section className='schedule'>
-				{/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+				{/* Replace this with the schedule elements during the "The Scheduler" activity. */}
 			</section>
 		</main>
 	);
