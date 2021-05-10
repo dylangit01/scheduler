efab163183483660e36a559acbbae9d7b60a6328
Dylan <dcren2010@gmail.com>
2021-05-10 13:45:35 -0400

    simply button classname condition without using lib

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,9 +4,8 @@ import "components/Button.scss";
 
 export default function Button({ children, onClick, disabled }) {
 	let buttonClass = 'button';
-	if (children) {
-		buttonClass += ` button--${children === 'Cancel' ? 'danger' : children.toLowerCase()}`;
-	}
+
+   children && (buttonClass += ` button--${children === 'Cancel' ? 'danger' : children.toLowerCase()}`)
 
 	return (
 		<button disabled={disabled} onClick={onClick} className={buttonClass}>
