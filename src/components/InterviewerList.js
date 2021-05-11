50df817b2339899913b301ddd8f40c0def3d340f
Dylan <dcren2010@gmail.com>
2021-05-11 08:38:15 -0400

    refactor Lists setInterviewer fn without pass down id

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './InterviewerList.scss';
+import InterviewerListItem from './InterviewerListItem';
+
+const InterviewerList = ({interviewers, interviewer, setInterviewer}) => {
+	return (
+		<section className='interviewers'>
+			<h4 className='interviewers__header text--light'>Interviewer</h4>
+			<ul className='interviewers__list'>
+				
+				{/* Remember setInterviewer fn is triggered in ListItem component with proper id, so we need to pass down the id,
+						but instead of passing setInterviewer fn as prop to ListItem, we can create call back fn with the id, 
+						so we no longer need to pass the id down to ListItem.  */}
+				{[...interviewers].map(({ id, name, avatar }) => (
+					<InterviewerListItem
+						key={id}
+						name={name}
+						avatar={avatar}
+						selected={id === interviewer}
+						setInterviewer={() => setInterviewer(id)}
+					/>
+				))}
+			</ul>
+		</section>
+	);
+}
+
+export default InterviewerList
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,14 +4,14 @@ import './InterviewerListItem.scss';
 // import classnames library:
 import classnames from 'classnames';
 
-const InterviewerListItem = ({ id, name, avatar, selected, setInterviewer }) => {
+const InterviewerListItem = ({ name, avatar, selected, setInterviewer }) => {
 
 	const interviewListClass = classnames('interviewers__item', {
 		'interviewers__item--selected': selected // selected is a boolean value passed from InterViewList
 	});
 
 	return (
-		<li className={interviewListClass} onClick={() => setInterviewer(name)}>
+		<li className={interviewListClass} onClick={setInterviewer}>
 			<img className='interviewers__item-image' src={avatar} alt={name} />
 			{selected && name}
 		</li>
