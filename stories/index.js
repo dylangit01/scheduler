f6b6970b50b73859e50979b60368e0cb4fe80546
Dylan <dcren2010@gmail.com>
2021-05-11 14:55:19 -0400

    setup Confirm Component with sbook

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from 'components/Appointment';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
 
 // Button
 storiesOf('Button', module)
@@ -141,4 +142,6 @@ storiesOf('Appointment', module)
 			onEdit={action('onEdit')}
 			onDelete={action('onDelete')}
 		/>
-	));
+	))
+	.add('Confirm', () => <Confirm message='Delete the appointment?' onConfirm={action('onConfirm')} onCancel={action('onCancel')} />);
+	
