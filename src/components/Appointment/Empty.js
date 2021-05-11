3fc376b9d1e3e4be357d2f119fc08611967fe679
Dylan <dcren2010@gmail.com>
2021-05-11 14:27:11 -0400

    build up the Empty component and setup sbook accordingly

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Empty = ({onAdd}) => {
+	return (
+		<main className='appointment__add'>
+			<img onClick={onAdd} className='appointment__add-button' src='images/add.png' alt='Add' />
+		</main>
+	);
+}
+
+export default Empty
