3f785790a1c9ab4274e4a5bccd65876562f7d834
Dylan <dcren2010@gmail.com>
2021-05-11 20:13:36 -0400

    Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from './Header';
-import './styles.scss'
+import Show from './Show';
+import Empty from './Empty';
+import './styles.scss';
 
-const Appointment = () => {
+const Appointment = ({ interview, time }) => {
 	return (
-		<article className="appointment">
-			<Header time='12pm'/>
+		<article className='appointment'>
+			<Header time={time} />
+			{interview ? 
+			<Show student={interview.student} interviewer={interview.interviewer} /> 
+			: 
+			<Empty />}
 		</article>
-	)
-}
+	);
+};
 
 export default Appointment;
