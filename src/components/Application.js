13d29cdbff7238513c2c781771f22240bd1a711c
Dylan <dcren2010@gmail.com>
2021-05-12 18:02:46 -0400

    combine state and refactor setDay and setDays fn with prev

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,60 +6,60 @@ import axios from 'axios';
 
 import 'components/Application.scss';
 
-const appointments = [
-	{
-		id: 1,
-		time: '12pm',
-	},
-	{
-		id: 2,
-		time: '1pm',
-		interview: {
-			student: 'Lydia Miller-Jones',
-			interviewer: {
-				id: 1,
-				name: 'Sylvia Palmer',
-				avatar: 'https://i.imgur.com/LpaY82x.png',
-			},
-		},
-	},
-	{
-		id: 3,
-		time: '10am',
-		interview: {
-			student: 'Dylan',
-			interviewer: {
-				id: 3,
-				name: 'Mildred Nazir',
-				avatar: 'https://i.imgur.com/T2WwVfS.png',
-			},
-		},
-	},
-	{
-		id: 4,
-		time: '3pm',
-		// interview: {
-		// 	student: 'Sienna',
-		// 	interviewer: {
-		// 		id: 5,
-		// 		name: 'Sven Jones',
-		// 		avatar: 'https://i.imgur.com/twYrpay.jpg',
-		// 	},
-		// },
-	},
-	{
-		id: 5,
-		time: '10am',
-		interview: {
-			student: 'Jack',
-			interviewer: {
-				id: 2,
-				name: 'Tori Malcolm',
-				avatar: 'https://i.imgur.com/Nmx0Qxo.png',
-			},
-		},
-	},
-];
+// const appointments = [
+// 	{
+// 		id: 1,
+// 		time: '12pm',
+// 	},
+// 	{
+// 		id: 2,
+// 		time: '1pm',
+// 		interview: {
+// 			student: 'Lydia Miller-Jones',
+// 			interviewer: {
+// 				id: 1,
+// 				name: 'Sylvia Palmer',
+// 				avatar: 'https://i.imgur.com/LpaY82x.png',
+// 			},
+// 		},
+// 	},
+// 	{
+// 		id: 3,
+// 		time: '10am',
+// 		interview: {
+// 			student: 'Dylan',
+// 			interviewer: {
+// 				id: 3,
+// 				name: 'Mildred Nazir',
+// 				avatar: 'https://i.imgur.com/T2WwVfS.png',
+// 			},
+// 		},
+// 	},
+// 	{
+// 		id: 4,
+// 		time: '3pm',
+// 		// interview: {
+// 		// 	student: 'Sienna',
+// 		// 	interviewer: {
+// 		// 		id: 5,
+// 		// 		name: 'Sven Jones',
+// 		// 		avatar: 'https://i.imgur.com/twYrpay.jpg',
+// 		// 	},
+// 		// },
+// 	},
+// 	{
+// 		id: 5,
+// 		time: '10am',
+// 		interview: {
+// 			student: 'Jack',
+// 			interviewer: {
+// 				id: 2,
+// 				name: 'Tori Malcolm',
+// 				avatar: 'https://i.imgur.com/Nmx0Qxo.png',
+// 			},
+// 		},
+// 	},
+// ];
 
 // const days = [
 // 	{
@@ -85,12 +85,12 @@ export default function Application(props) {
 
 	// Combine states:
 	const [state, setState] = useState({
-		day: "Monday",
+		day: 'Monday',
 		days: [],
 		// appointments:{}
-	})
-	const setDay = day => setState({ ...state, day });
-	const setDays = days => setState({ ...state, days });
+	});
+	const setDay = (day) => setState({ ...state, day });
+	const setDays = (days) => setState((prev) => ({ ...prev, days }));		// why useEffect not complain now?
 
 	useEffect(() => {
 		const ENDPOINT_DAY = 'http://localhost:8001/api/days';
