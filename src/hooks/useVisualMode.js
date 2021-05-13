e12ce5ba3ee1287c30a02efc0b0aa4edda45eb7b
Dylan <dcren2010@gmail.com>
2021-05-13 13:17:20 -0400

    building a cutom Hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,15 +4,20 @@ export default function useVisualMode(initial) {
 	const [mode, setMode] = useState(initial);
 	const [history, setHistory] = useState([initial]);
 
-	const transition = function (mode) {
-		setMode(mode);
-		setHistory([...history, mode]);
+	const transition = function (mode, replace = false) {
+		if (replace) {
+			setHistory([history.pop(), ...history, mode]);
+			setMode(mode);
+		} else {
+			setMode(mode);
+			setHistory([...history, mode]);
+		}
 	};
 
 	const back = function () {
 		if (history.length > 1) {
-			history.pop();
-			// setHistory([...history])
+			// history.pop();			// Is this bed practice for pop history directly?
+			setHistory([history.pop(), ...history]);
 			setMode(history[history.length - 1]);
 		}
 	};
