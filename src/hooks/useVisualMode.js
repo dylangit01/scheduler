f15687f760720322f3863e917f89902f83b8c391
Dylan <dcren2010@gmail.com>
2021-05-13 10:02:45 -0400

    add custom hooks-useVisualMode with the jest tests

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+	const [mode, setMode] = useState(initial);
+	const [history, setHistory] = useState([initial]);
+
+	const transition = function (mode) {
+		setMode(mode);
+		setHistory([...history, mode]);
+	};
+
+	const back = function () {
+		if (history.length > 1) {
+			history.pop();
+			// setHistory([...history])
+			setMode(history[history.length - 1]);
+		}
+	};
+
+	return { mode, transition, back };
+}
