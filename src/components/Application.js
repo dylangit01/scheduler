332a71384f55747d4f7c90d9771760e88156e7a0
Dylan <dcren2010@gmail.com>
2021-05-13 07:42:25 -0400

    remove hardcorded database

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,61 +6,6 @@ import { getAppointmentsForDay, getInterview } from '../helpers/selectors';
 
 import 'components/Application.scss';
 
-// const appointments = [
-// 	{
-// 		id: 1,
-// 		time: '12pm',
-// 	},
-// 	{
-// 		id: 2,
-// 		time: '1pm',
-// 		interview: {
-// 			student: 'Lydia Miller-Jones',
-// 			interviewer: {
-// 				id: 1,
-// 				name: 'Sylvia Palmer',
-// 				avatar: 'https://i.imgur.com/LpaY82x.png',
-// 			},
-// 		},
-// 	},
-// 	{
-// 		id: 3,
-// 		time: '10am',
-// 		interview: {
-// 			student: 'Dylan',
-// 			interviewer: {
-// 				id: 3,
-// 				name: 'Mildred Nazir',
-// 				avatar: 'https://i.imgur.com/T2WwVfS.png',
-// 			},
-// 		},
-// 	},
-// 	{
-// 		id: 4,
-// 		time: '3pm',
-// 		// interview: {
-// 		// 	student: 'Sienna',
-// 		// 	interviewer: {
-// 		// 		id: 5,
-// 		// 		name: 'Sven Jones',
-// 		// 		avatar: 'https://i.imgur.com/twYrpay.jpg',
-// 		// 	},
-// 		// },
-// 	},
-// 	{
-// 		id: 5,
-// 		time: '10am',
-// 		interview: {
-// 			student: 'Jack',
-// 			interviewer: {
-// 				id: 2,
-// 				name: 'Tori Malcolm',
-// 				avatar: 'https://i.imgur.com/Nmx0Qxo.png',
-// 			},
-// 		},
-// 	},
-// ];
-
 export default function Application(props) {
 	// const [day, setDay] = useState('Monday');
 	// const [days, setDays] = useState([]);
@@ -73,7 +18,9 @@ export default function Application(props) {
 		interviewers: {},
 	});
 	// setDay function can remain because we are only using it to update our DayList component.
-	const setDay = (day) => setState({ ...state, day });
+	// const setDay = (day) => setState((prev) => ({ ...prev, day }));
+	const setDay = (day) => setState({...state, day});
+	
 	// setDays fn will be refactored with fetch appointments fn:
 	// const setDays = (days) => setState((prev) => ({ ...prev, days }));		// why useEffect not complain now?
 
