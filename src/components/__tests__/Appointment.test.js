80639949406d387714f3b51b1d220cb1e700fbf3
Dylan <dcren2010@gmail.com>
2021-05-17 01:03:06 -0400

    create appointment.test.js file

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,14 @@
+// Change the watch mode to p and type in Appointment to only run the Appointment.test.js file after each update
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Appointment from 'components/Application';
+
+afterEach(cleanup);
+
+describe('Appointment', () => {
+	it('renders without crashing', () => {
+		render(<Appointment />);
+	});
+});
