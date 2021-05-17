5b0006695a7c74f045f4f10b372a4923d3b1efe4
Dylan <dcren2010@gmail.com>
2021-05-17 16:32:15 -0400

    Mocking modules for application & appointment

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,13 +1,20 @@
 // Change the watch mode to p and type in Appointment to only run the Appointment.test.js file after each update
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Appointment from 'components/Application';
 
 afterEach(cleanup);
 
 describe('Appointment', () => {
-	it('renders without crashing', () => {
-		render(<Appointment />);
+	it('defaults to Monday and changes the appointment interview when a new day is selected', () => {
+		const { getByText } = render(<Appointment />);
+		return waitForElement(() => getByText('Monday')).then(() => {
+			fireEvent.click(getByText('Tuesday'));
+			expect(getByText('Leopold Silvers')).toBeInTheDocument();
+		})
 	});
+
+
+
 });
