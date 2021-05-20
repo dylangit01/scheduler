433c9f9ee4977095d4a35d5aa551d83021d6c477
Dylan <dcren2010@gmail.com>
2021-05-20 07:21:15 -0400

    add loading indicator for initial fetching data from heroku

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DayList from './DayList';
 import Appointment from './Appointment';
+import Status from '../components/Appointment/Status';
 
 import { getAppointmentsForDay, getInterviewersForDay, getInterview } from '../helpers/selectors';
 // import useApplicationData from '../hooks/useApplicationData';			//-->useState
@@ -12,44 +13,49 @@ export default function Application(props) {
 	const { state, setDay, bookInterview, cancelInterview } = useApplicationDataRefactor();
 
 	return (
-		<main className='layout'>
-			<section className='sidebar'>
-				<img className='sidebar--centered' src='images/logo.png' alt='Interview Scheduler' />
-				<hr className='sidebar__separator sidebar--centered' />
-				<nav className='sidebar__menu'>
+		<>
+			{state.days.length > 0 ? (
+				<main className='layout'>
+					<section className='sidebar'>
+						<img className='sidebar--centered' src='images/logo.png' alt='Interview Scheduler' />
+						<hr className='sidebar__separator sidebar--centered' />
+						<nav className='sidebar__menu'>
 
-					{/* setDay fn has been passed down to DayList, and passed down to DayListItem again, 
-					because the trigger event is from DayListItem, the obtained day value will be retrieved 
-					from DayListItem setDay fn, and logged here.  */}
+							{/* setDay fn has been passed down to DayList, and passed down to DayListItem again, 
+								because the trigger event is from DayListItem, the obtained day value will be retrieved 
+								from DayListItem setDay fn, and logged here.  */}
 
-					<DayList
-						days={state.days}
-						value={state.day} // Use generic way (value and onChange) to pass down day and setSelectedDay
-						onChange={setDay}
-					/>
-				</nav>
-				<img className='sidebar__lhl sidebar--centered' src='images/lhl.png' alt='Lighthouse Labs' />
-			</section>
-			<section className='schedule'>
-
-				{/* using help fn to return the array of appointments */}
-				{getAppointmentsForDay(state, state.day).map((appointment) => {
-					const interview = getInterview(state, appointment.interview);
-					const interviewers = getInterviewersForDay(state, state.day);
-					return (
-						<Appointment
-							key={appointment.id}
-							{...appointment}
-							interview={interview}
-							interviewers={interviewers}
-							bookInterview={bookInterview}
-							cancelInterview={cancelInterview}
-						/>
-					);
-				})}
-				<Appointment bookInterview={bookInterview} cancelInterview={cancelInterview} key='last' time='5pm' />
-			</section>
-		</main>
+							<DayList
+								days={state.days}
+								value={state.day} // Use generic way (value and onChange) to pass down day and setSelectedDay
+								onChange={setDay}
+							/>
+						</nav>
+						<img className='sidebar__lhl sidebar--centered' src='images/lhl.png' alt='Lighthouse Labs' />
+					</section>
+					<section className='schedule'>
+						{/* using help fn to return the array of appointments */}
+						{getAppointmentsForDay(state, state.day).map((appointment) => {
+							const interview = getInterview(state, appointment.interview);
+							const interviewers = getInterviewersForDay(state, state.day);
+							return (
+								<Appointment
+									key={appointment.id}
+									{...appointment}
+									interview={interview}
+									interviewers={interviewers}
+									bookInterview={bookInterview}
+									cancelInterview={cancelInterview}
+								/>
+							);
+						})}
+						<Appointment bookInterview={bookInterview} cancelInterview={cancelInterview} key='last' time='5pm' />
+					</section>
+				</main>
+			) : (
+				<Status message='Data loading...' />
+			)}
+		</>
 	);
 }
 
